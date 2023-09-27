5d6965d768ca9dbe70ff4e8280231f837934d598
Coderner <shreyashukla680@gmail.com>
2023-09-27 16:26:26 +0530

    added routing using create Browser Router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import "./App.css";
 import Home from "./components/Home";
 import Create from "./components/Create"
 import Dashboard from "./components/Dashboard";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const appRouter = createBrowserRouter([{
+  path:"/",
+  element:<Home/>
+},
+{
+  path:"/dashboard",
+  element:<Dashboard/>
+},
+{
+  path:"/create",
+  element:<Create/>
+}])
 
 function App() {
+
   return (
     <div>
-       <Dashboard/>
+       <RouterProvider router={appRouter}/>
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {FaClipboardList, FaUserFriends, FaPlusCircle} from 'react-icons/fa';
 import TableData from "./TableData";
+import { Link } from "react-router-dom";
 
 const Dashboard = () =>{
     
@@ -21,11 +22,11 @@ const Dashboard = () =>{
         <div className="bg-fuchsia-100 pb-20">
             <div className="flex justify-between px-16">
                 <h1 className="flex text-3xl py-9 px-14 font-semibold text-purple-700"><FaClipboardList className="mt-1"/> Dashboard</h1>
-                <div className=" py-9 px-14"><button className="flex text-xl bg-green-500 font-semibold py-2 px-4 text-white rounded-2xl"> Create New User Profile <FaPlusCircle className="mt-1 ml-1"/></button></div>
+                <div className=" py-9 px-14"><Link to="/create"><button className="flex text-xl bg-green-500 font-semibold py-2 px-4 text-white rounded-2xl"> Create New User Profile <FaPlusCircle className="mt-1 ml-1"/></button></Link></div>
             </div>
             <div className="w-5/6 bg-fuchsia-200 mx-auto shadow-lg shadow-fuchsia-200 rounded-2xl p-6">
             <div>
-                <div className="flex">
+                <div className="flex m-2">
                     <div className="w-1/6 text-center font-medium">Name</div>
                     <div className="w-1/6 text-center font-medium">UserName</div>
                     <div className="w-1/6 text-center font-medium">Email</div>
